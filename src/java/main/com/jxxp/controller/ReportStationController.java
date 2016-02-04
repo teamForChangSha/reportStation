efe56ba986c30b83b124471ad1b03d26ea020a9a
git123xia <820786562@qq.com>
2016-02-04 11:36:59 +0800

    controller 参数修正

--- src/java/main/com/jxxp/controller/ReportStationController.java
+++ src/java/main/com/jxxp/controller/ReportStationController.java
@@ -12,10 +12,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.alibaba.druid.util.StringUtils;
 import com.alibaba.fastjson.JSONArray;
 import com.jxxp.pojo.AreaInfo;
-import com.jxxp.pojo.CompanyBranch;
 import com.jxxp.service.AreaService;
 import com.jxxp.service.CompanyService;
 
@@ -26,27 +24,26 @@ public class ReportStationController {
 	private CompanyService companyService;
 	@Resource
 	private AreaService areaService;
-	
+
 	@RequestMapping("/index.do")
-	public String getCompany(HttpServletRequest request,
-			HttpServletResponse response, ModelMap modelMap) {
+	public String getCompany(HttpServletRequest request, HttpServletResponse response,
+			ModelMap modelMap) {
 		modelMap.put("companyList", companyService.getAllCompanyList());
 		return "jsp/index";
 	}
-	
 
 	@RequestMapping("/getProvinceByCompany.do")
-	public String getProvinceByCompanyId(HttpServletRequest request,
-			HttpServletResponse response, ModelMap modelMap) {
+	public String getProvinceByCompanyId(HttpServletRequest request, HttpServletResponse response,
+			ModelMap modelMap) {
 		long companyId = 0;
-		
+
 		String cId = request.getParameter("companyId");
-		if(cId != null) {
+		if (cId != null) {
 			companyId = Long.parseLong(cId);
 		}
-		
+
 		List<AreaInfo> dataList = areaService.getProvinceByCompanyId(companyId);
-		
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
@@ -58,24 +55,24 @@ public class ReportStationController {
 		}
 		return null;
 	}
-	
+
 	@RequestMapping("/getCityByCompany.do")
-	public String getCityByCompanyId(HttpServletRequest request,
-			HttpServletResponse response, ModelMap modelMap) {
+	public String getCityByCompanyId(HttpServletRequest request, HttpServletResponse response,
+			ModelMap modelMap) {
 		long companyId = 0;
-		long areaId = 0;
-		
+		long parentId = 0;
+
 		String cId = request.getParameter("companyId");
-		String aId = request.getParameter("areaId");
-		if(cId != null) {
+		String aId = request.getParameter("parentId");
+		if (cId != null) {
 			companyId = Long.parseLong(cId);
 		}
-		if(aId != null) {
-			areaId = Long.parseLong(aId);
+		if (aId != null) {
+			parentId = Long.parseLong(aId);
 		}
-		
-		List<AreaInfo> dataList = areaService.getCityByCompanyId(companyId, areaId);
-		
+
+		List<AreaInfo> dataList = areaService.getCityByCompanyId(companyId, parentId);
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
@@ -87,34 +84,35 @@ public class ReportStationController {
 		}
 		return null;
 	}
-	
-//	@RequestMapping("/getCompanyBranch.do")
-//	public String getCompanyBranch(HttpServletRequest request,
-//			HttpServletResponse response, ModelMap modelMap) {
-//		long companyId = 0;
-//		long areaId = 0;
-//		
-//		String cId = request.getParameter("companyId");
-//		String aId = request.getParameter("areaId");
-//		if(cId != null) {
-//			companyId = Long.parseLong(cId);
-//		}
-//		if(aId != null) {
-//			areaId = Long.parseLong(aId);
-//		}
-//		
-//		List<CompanyBranch> dataList = companyService.getCompanyBranchByArea(area, company);
-//		
-//		response.setCharacterEncoding("UTF-8");
-//		PrintWriter out;
-//		try {
-//			out = response.getWriter();
-//			String json = JSONArray.toJSONString(dataList);
-//			out.print(json);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return null;
-//	}
-	
+
+	// @RequestMapping("/getCompanyBranch.do")
+	// public String getCompanyBranch(HttpServletRequest request,
+	// HttpServletResponse response, ModelMap modelMap) {
+	// long companyId = 0;
+	// long areaId = 0;
+	//
+	// String cId = request.getParameter("companyId");
+	// String aId = request.getParameter("areaId");
+	// if(cId != null) {
+	// companyId = Long.parseLong(cId);
+	// }
+	// if(aId != null) {
+	// areaId = Long.parseLong(aId);
+	// }
+	//
+	// List<CompanyBranch> dataList =
+	// companyService.getCompanyBranchByArea(area, company);
+	//
+	// response.setCharacterEncoding("UTF-8");
+	// PrintWriter out;
+	// try {
+	// out = response.getWriter();
+	// String json = JSONArray.toJSONString(dataList);
+	// out.print(json);
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// return null;
+	// }
+
 }
