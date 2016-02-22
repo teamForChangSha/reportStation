4ac15f21548aa58894f27943eacc6a7c2fd28235
sundy <178972841@qq.com>
2016-02-22 10:53:01 +0800

    完成首页功能

--- WebContent/jsp/js/index.js
+++ WebContent/jsp/js/index.js
@@ -1,258 +1,276 @@
 ﻿$(function() {
-	var etpList = $("#etpList");
+
+	var leftEle = {
+		companys : $("#companys"),
+		province : $("select[name=province]"),
+		city : $("select[name=city]"),
+		institutions : $("select[name=branchId]"),
+		companyName : $("input[name=companyName]"),
+		getArea : $("#getArea"),
+		next : $("#next")
+	};
+
+	var rightEle = {
+		trankingNo : $("input[name=trankingNo]"),
+		accecCode : $("input[name=accecCode]"),
+		getCaseInfoByNo : $("#getCaseInfoByNo"),
+		mobile : $("input[name=mobile]"),
+		getTempPwd : $("#getTempPwd"),
+		tempPwd : $("input[name=tempPwd]"),
+		getCaseInfoByPhone : $("#getCaseInfoByPhone")
+	};
+
+	var regStr = /(1[3-9]\d{9}$)/;
+	var t, i = 60;
+
+	/**
+	 * 获取所有企业
+	 */
 	$.getJSON("getAllCompany.do", function(res) {
 		if (res == null || res.length < 0)
 			return;
 		$.each(res, function(i, company) {
-			console.log(company.companyName);
 			var li = $("<li/>");
 			var a = $("<a/>").attr("data-id", company.companyId).text(
 					company.companyName);
 			a.click(function() {
 				hiddenEle();
-				keyword.val(this.innerHTML);
-				companyId.val($(this).attr("data-id"));
-				changeClass(keyword, keywordIcon, false);
+				leftEle.companyName.val(this.innerHTML);
+				leftEle.companyName.attr("data-id", $(this).attr("data-id"));
+				hiddenErrorIcon(leftEle.companyName);
 			});
 			li.append(a);
-			etpList.append(li);
+			leftEle.companys.append(li);
 		});
 	});
-	
-	var addSelOption = function(jq) {
-		var opt = $("<option/>").text("请选择").attr("value", "-1");
-		jq.append(opt);
-	};
 
-	var province = $("#seachprov"), city = $("#seachcity"), institutions = $("#institutions"), companyId = $("input[name=companyId]");
-
+	/**
+	 * 初始化省份
+	 */
 	function initProvince() {
-		var url = "getProvinceByCompany.do?companyId=" + companyId.val();
+		var url = "getProvinceByCompany.do?companyId="
+				+ leftEle.companyName.attr("data-id");
 		$.getJSON(url, function(result) {
-			if (result == null || result.length < 0) {
+			if (result == null || result.length <= 0) {
 				return;
 			}
-			province.empty();
+			leftEle.province.empty();
 			$(result).each(
 					function() {
 						var opt = $("<option/>").text(this.name).attr("value",
 								this.areaId);
-						province.append(opt);
+						leftEle.province.append(opt);
 					});
+			leftEle.province.parent().removeClass("hidden");
 			initCity();
 		});
 	}
 	;
 
-	var initCity = function() {
-		var url = "getCityByCompany.do?companyId=" + companyId.val()
-				+ "&parentId=" + $("#seachprov  option:selected").val();
+	/**
+	 * 初始化市
+	 */
+	function initCity() {
+		var url = "getCityByCompany.do?companyId="
+				+ leftEle.companyName.attr("data-id") + "&parentId="
+				+ leftEle.province.find("option:selected").val();
 		$.getJSON(url, function(result) {
-			if (result == null || result.length < 0) {
+			if (result == null || result.length <= 0) {
 				return;
 			}
-			city.empty();
+			leftEle.city.empty();
 			$(result).each(
 					function() {
 						var opt = $("<option/>").text(this.name).attr("value",
 								this.areaId);
-						city.append(opt);
+						leftEle.city.append(opt);
 					});
-			city.parent().removeClass("hidden");
+			leftEle.city.parent().removeClass("hidden");
 			initInstitutions();
 		});
-	};
+	}
+	;
 
-	var initInstitutions = function() {
-		var url = "getCompanyBranch.do?companyId=" + companyId.val()
-				+ "&areaId=" + $("#seachcity  option:selected").val();
+	/**
+	 * 初始化机构
+	 */
+	function initInstitutions() {
+		var url = "getCompanyBranch.do?companyId="
+				+ leftEle.companyName.attr("data-id") + "&areaId="
+				+ leftEle.city.find("option:selected").val();
 		$.getJSON(url, function(result) {
-			if (result == null || result.length < 0) {
+			if (result == null || result.length <= 0) {
 				return;
 			}
-			institutions.empty();
+			leftEle.institutions.empty();
 			$(result).each(
 					function() {
 						var opt = $("<option/>").text(this.branchName).attr(
 								"value", this.branchId);
-						institutions.append(opt);
+						leftEle.institutions.append(opt);
 					});
-			institutions.parent().removeClass("hidden");
-			next.parent().removeClass("hidden");
+			leftEle.institutions.parent().removeClass("hidden");
+			leftEle.next.parent().removeClass("hidden");
 		});
-	};
-
-	var setData = function(data, ele) {
-		ele.empty();
-		$(data).each(
-				function() {
-					var opt = $("<option/>").text(this.name).attr("value",
-							this.areaId);
-					ele.append(opt);
-				});
 	}
+	;
 
-	province.change(function() {
+	/**
+	 * 获取企业下的机构
+	 */
+	leftEle.getArea.click(function() {
+		showErrorIcon(leftEle.companyName)
+		initProvince();
+	});
+
+	/**
+	 * 转到企业问题类型页面
+	 */
+	leftEle.next.click(function() {
+		$("#sendCompany").submit();
+	});
+
+	leftEle.province.change(function() {
+		leftEle.city.parent().addClass("hidden");
+		leftEle.institutions.parent().addClass("hidden");
 		initCity();
 	});
 
-	city.change(function() {
+	leftEle.city.change(function() {
+		leftEle.institutions.parent().addClass("hidden");
 		initInstitutions();
 	});
 
-	var num = $("#num"), phone = $("input[name=mobile]"), pwd = $("input[name=tempPwd]"), keyword = $("#keyword");
-	var numIcon = $("#numIcon"), phoneIcon = $("#phoneIcon"), pwdIcon = $("#pwdIcon"), keywordIcon = $("#keywordIcon");
-	var numBtn = $("#numBtn"), phoneBtn = $("#phoneBtn"), pwdBtn = $("#pwdBtn"), getArea = $("#getArea"), next = $("#next");
-	var regStr = /(1[3-9]\d{9}$)/;
-	var t, i = 60;
+	leftEle.companyName.get(0).onkeyup = hiddenEle;
+	hiddenErrorIcon(leftEle.companyName);
+	hiddenErrorIcon(rightEle.trankingNo);
+	hiddenErrorIcon(rightEle.accecCode);
+	hiddenErrorIcon(rightEle.mobile);
+	hiddenErrorIcon(rightEle.tempPwd);
 
-	changeClass(num, numIcon, false);
-	changeClass(pwd, pwdIcon, false);
-	changeClass(phone, phoneIcon, false);
-	changeClass(keyword, keywordIcon, false);
-	changeSelectClass(province, false);
-	changeSelectClass(city, false);
 	/**
-	 * 判断手机号是否合法
-	 * 
-	 * @param {Object}
-	 *            inp
-	 * @param {Object}
-	 *            iconId
+	 * 隐藏企业机构控件
 	 */
-	function reg(ele, iconId) {
-		if (!regStr.test($.trim(ele.val()))) {
-			iconId.addClass("glyphicon-remove");
+	function hiddenEle() {
+		leftEle.companyName.removeAttr("data-id");
+		leftEle.province.parent().addClass("hidden");
+		leftEle.city.parent().addClass("hidden");
+		leftEle.institutions.parent().addClass("hidden");
+		leftEle.next.parent().addClass("hidden");
+	}
+	;
+
+	rightEle.trankingNo.focus(function() {
+		rightEle.accecCode.removeAttr("disabled");
+		rightEle.getCaseInfoByNo.removeAttr("disabled");
+		rightEle.getTempPwd.attr("disabled", "");
+		rightEle.tempPwd.attr("disabled", "");
+		rightEle.getCaseInfoByPhone.attr("disabled", "");
+	});
+	rightEle.mobile.focus(function() {
+		rightEle.accecCode.attr("disabled", "");
+		rightEle.getCaseInfoByNo.attr("disabled", "");
+		rightEle.getTempPwd.removeAttr("disabled");
+		rightEle.tempPwd.removeAttr("disabled");
+		rightEle.getCaseInfoByPhone.removeAttr("disabled");
+	});
+
+	/**
+	 * 通过案件编号查询
+	 */
+	rightEle.getCaseInfoByNo.click(function() {
+		if (!showErrorIcon(rightEle.trankingNo)
+				&& !showErrorIcon(rightEle.accecCode)) {
+			$("#reportPanel").modal('hide');
+			$("#selectByNum").submit();
+		}
+	});
+
+	/**
+	 * 获取临时密码
+	 */
+	rightEle.getTempPwd.click(function() {
+		if (!reg(rightEle.mobile)) {
+			return;
+		}
+		rightEle.getTempPwd.attr("disabled", true);
+		rightEle.getTempPwd.html("(" + i + ")秒后重试");
+		t = setInterval(function() {
+			if (i == 0) {
+				clearInterval(t);
+				t = null;
+				rightEle.getTempPwd.removeAttr("disabled");
+				rightEle.getTempPwd.html("获取临时密码");
+				i = 60;
+			} else {
+				rightEle.getTempPwd.attr("disabled", true);
+				i--;
+				rightEle.getTempPwd.html("(" + i + ")秒后重试");
+			}
+		}, 1000);
+	});
+
+	/**
+	 * 通过实名手机号查询
+	 */
+	rightEle.getCaseInfoByPhone.click(function() {
+		if (reg(rightEle.mobile) && !showErrorIcon(rightEle.tempPwd)) {
+			clearInterval(t);
+			t = null;
+			rightEle.getTempPwd.removeAttr("disabled");
+			rightEle.getTempPwd.html("获取临时密码");
+			i = 60;
+			$("#reportPanel").modal('hide');
+			$("#selectByPhone").submit();
+		}
+	});
+
+	/**
+	 * 验证手机号
+	 */
+	function reg(ele) {
+		if (!regStr.test(ele.val())) {
+			ele.next().addClass("glyphicon-remove");
 			return false;
 		} else {
 			return true;
 		}
 	}
+
+	/**
+	 * 显示错误icon
+	 */
+	function showErrorIcon(ele) {
+		if (isEmty(ele.val())) {
+			ele.next().addClass("glyphicon-remove");
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 隐藏错误icon
+	 */
+	function hiddenErrorIcon(ele) {
+		ele.focus(function() {
+			ele.next().removeClass("glyphicon-remove");
+		});
+		ele.next().removeClass("glyphicon-remove");
+	}
+
 	/**
 	 * 判断是否为空
 	 * 
-	 * @param {Object}
-	 *            inp
-	 * @param {Object}
-	 *            iconId
-	 * @param {Object}
-	 *            bool
+	 * @param str
+	 * @returns {Boolean}
 	 */
-	function changeClass(ele, iconId, bool) {
-		if (bool) {
-			if ($.trim(ele.val()) == null || $.trim(ele.val()) == ""
-					|| $.trim(ele.val()).length <= 0) {
-				iconId.addClass("glyphicon-remove");
-				return false;
-			} else {
-				return true;
-			}
-		} else {
-			ele.focus(function() {
-				iconId.removeClass("glyphicon-remove");
-				return;
-			});
-			iconId.removeClass("glyphicon-remove");
+	function isEmty(str) {
+		str = $.trim(str);
+		if (str == null || str.length <= 0 || str == "") {
+			return true;
 		}
+		return false;
 	}
 
-	function changeSelectClass(ele, bool) {
-		if (bool) {
-			if (ele.val() == "-1" || ele.val() == "0") {
-				ele.parent().addClass("has-error");
-				return false;
-			} else {
-				return true;
-			}
-		} else {
-			ele.change(function() {
-				if (ele.val() != "-1" && ele.val() != "0") {
-					ele.parent().removeClass("has-error");
-				} else {
-					ele.parent().addClass("has-error");
-				}
-			});
-		}
-	}
-
-	keyword.get(0).onkeyup = hiddenEle;
-	
-	function hiddenEle() {
-		province.parent().addClass("hidden");
-		city.parent().addClass("hidden");
-		institutions.parent().addClass("hidden");
-		next.parent().addClass("hidden");
-	};
-
-	/**
-	 * 根据单号匿名查询
-	 */
-	numBtn.click(function() {
-		if (changeClass(num, numIcon, true)) {
-			$("#reportPanel").modal('hide');
-			 $("#selectByNum").submit();
-//			location.href = "userPages/report_info.html";
-		}
-	});
-	/**
-	 * 获取临时密码
-	 */
-	phoneBtn.click(function() {
-		if (!reg(phone, phoneIcon)) {
-			return;
-		}
-		phoneBtn.attr("disabled", true);
-		phoneBtn.html("(" + i + ")秒后重试");
-		t = setInterval(function() {
-			if (i == 0) {
-				clearInterval(t);
-				t = null;
-				phoneBtn.removeAttr("disabled");
-				phoneBtn.html("获取临时密码");
-				i = 60;
-			} else {
-				phoneBtn.attr("disabled", true);
-				i--;
-				phoneBtn.html("(" + i + ")秒后重试");
-			}
-		}, 1000);
-	})
-	/**
-	 * 发送查询请求
-	 */
-	pwdBtn.click(function() {
-		alert("手机号"+phone.val());
-		if (reg(phone, phoneIcon) && changeClass(pwd, pwdIcon, true)) {
-			clearInterval(t);
-			t = null;
-			phoneBtn.removeAttr("disabled");
-			phoneBtn.html("获取临时密码");
-			i = 60;
-			$("#reportPanel").modal('hide');
-			 $("#selectByPhone").submit();
-			 
-		}
-	});
-	/**
-	 * 选择企业发送举报请求
-	 */
-	getArea.click(function() {
-		if (changeClass(keyword, keywordIcon, true)) {
-			province.parent().removeClass("hidden");
-			initProvince();
-		}
-	});
-	next.click(function() {
-		if (changeClass(keyword, keywordIcon, true)
-				&& changeSelectClass(province, true)
-				&& changeSelectClass(city, true)) {
-			$("#sendCompany").submit();
-		}
-	});
-	/**
-	 * 跳转到找回密码页面
-	 */
-	$("#forgetPwd").click(function() {
-		$("#loginPanel").modal('hide');
-	});
 });
