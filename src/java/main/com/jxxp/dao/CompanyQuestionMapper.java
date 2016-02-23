7fa165e11c83857ba916af3ae1dcda02eae2d3cf
git123xia <820786562@qq.com>
2016-02-23 09:46:26 +0800

    问题与公司关联表的mapper和xml

--- src/java/main/com/jxxp/dao/CompanyQuestionMapper.java
+++ src/java/main/com/jxxp/dao/CompanyQuestionMapper.java
@@ -5,5 +5,16 @@ import org.apache.ibatis.annotations.Param;
 public interface CompanyQuestionMapper {
 	int insert(@Param("questId") long questId, @Param("companyId") long companyId);
 
+	/**
+	 * 保存公司所选则的问题
+	 * 
+	 * @param questIds
+	 *            问题id数组
+	 * @param companyId
+	 *            公司id
+	 * @return
+	 */
+	int insertAll(@Param("questIds") long[] questIds, @Param("companyId") long companyId);
+
 	int deleteByDoubleId(@Param("questId") long questId, @Param("companyId") long companyId);
 }
