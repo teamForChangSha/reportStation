bc391a8355b5b58d6b57a9ffc0d7c09b911da495
windsword017 <9466672@qq.com>
2016-02-26 10:44:35 +0800

    修改了增加追加信息的页面返回方式
    Signed-off-by: windsword017 <9466672@qq.com>

--- src/java/main/com/jxxp/controller/CaseController.java
+++ src/java/main/com/jxxp/controller/CaseController.java
@@ -424,38 +424,32 @@ public class CaseController {
 			caseComment.setOwnerCompany(user.getUserCompany());
 		}
 		
-		response.setCharacterEncoding("UTF-8");
-		PrintWriter out;
-		try {
-			out = response.getWriter();
-			if(caseCommentService.addCaseComment(caseComment, rcId)){
-				log.debug("案件追加信息添加成功！");
-				//如果不是举报人追加信息，需要记录变更信息
-				if(caseComment.getIsReporter() == 0) {
-					CaseChangeLog caseChangeLog = new CaseChangeLog();
-					caseChangeLog.setChangeTime(new Date());
-					caseChangeLog.setOperator(user);
-					caseChangeLog.setStateAfter(reportCase.getCaseState());
-					caseChangeLog.setStateBefore(reportCase.getCaseState());
-					caseChangeLog.setHandlerAfter(user.getUserCompany());
-					caseChangeLog.setHandlerBefore(user.getUserCompany());
-					
-					if(caseChangeLogService.addCaseChangeLog(caseChangeLog, rcId)) {
-						log.debug("日志信息添加成功！");
-					} else {
-						log.debug("日志信息添加失败！");
-					}
+		if(caseCommentService.addCaseComment(caseComment, rcId)){
+			log.debug("案件追加信息添加成功！");
+			//如果不是举报人追加信息，需要记录变更信息
+			if(caseComment.getIsReporter() == 0) {
+				CaseChangeLog caseChangeLog = new CaseChangeLog();
+				caseChangeLog.setChangeTime(new Date());
+				caseChangeLog.setOperator(user);
+				caseChangeLog.setStateAfter(reportCase.getCaseState());
+				caseChangeLog.setStateBefore(reportCase.getCaseState());
+				caseChangeLog.setHandlerAfter(user.getUserCompany());
+				caseChangeLog.setHandlerBefore(user.getUserCompany());
+				
+				if(caseChangeLogService.addCaseChangeLog(caseChangeLog, rcId)) {
+					log.debug("日志信息添加成功！");
+				} else {
+					log.debug("日志信息添加失败！");
 				}
-				out.print("success");
-			} else {
-				log.debug("案件追加信息添加失败！");
-				out.print("error");
 			}
-		} catch (IOException e) {
-			log.error("流获取失败！",e);
+		} else {
+			log.debug("案件追加信息添加失败！");
 		}
 		
-    	return null;
+		modelMap.put("reportCase", reportCase);
+		modelMap.put("questionAnswerList", getQuestionAnswerList(reportCase));
+		
+    	return "/jsp/pages/report_info";
     }
     
     /*** 
