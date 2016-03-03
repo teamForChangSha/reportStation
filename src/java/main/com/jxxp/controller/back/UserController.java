38901352e0ce6a2f425d3d51fdbc89cd8613b635
git123xia <820786562@qq.com>
2016-03-03 11:57:20 +0800

    用户模糊查询更改bug

--- src/java/main/com/jxxp/controller/back/UserController.java
+++ src/java/main/com/jxxp/controller/back/UserController.java
@@ -24,28 +24,28 @@ import com.jxxp.pojo.OprationLog;
 import com.jxxp.pojo.User;
 import com.jxxp.service.OprationLogService;
 import com.jxxp.service.UserService;
+
 /*
  * 用户Controller
  * @author cj
  */
 @Controller("userController")
 @RequestMapping("/admin/user")
-public class UserController {	
+public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
 	@Resource
 	private UserService userService;
 	@Resource
 	private OprationLogService oprationLogService;
-	
-	
+
 	/*
 	 * 用户登录
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/login.do")
-	public String login( HttpServletRequest request, HttpServletResponse response,
-			ModelMap modelMap) {
+	public String login(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
 		String loginName = request.getParameter("loginName");
 		String userPwd = request.getParameter("userPwd");
 		User user = new User();
@@ -53,28 +53,29 @@ public class UserController {
 		user.setUserPwd(userPwd);
 		user = userService.longin(user);
 		log.debug("User:" + user);
-		
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(user != null) {
-				if(user.getUserState() != 1) {
+			if (user != null) {
+				if (user.getUserState() != 1) {
 					out.print("该账号无法使用！");
 				} else {
 					ServletContext application = request.getSession().getServletContext();
 					@SuppressWarnings("unchecked")
-					Map<String, String> loginUsers = (Map<String, String>) application.getAttribute("loginUsers");
-					
+					Map<String, String> loginUsers = (Map<String, String>) application
+							.getAttribute("loginUsers");
+
 					String sessionId = request.getSession(false).getId();
 					log.debug("loginUsers:" + loginUsers);
-					if(loginUsers == null || loginUsers.size() == 0) {
+					if (loginUsers == null || loginUsers.size() == 0) {
 						loginUsers = new HashMap<String, String>();
 						loginUsers.put(user.getLoginName(), sessionId);
 						application.setAttribute("loginUsers", loginUsers);
-						
+
 						request.getSession().setAttribute("user", user);
-						if(saveOprationLog("登录", user.getUserId())) {
+						if (saveOprationLog("登录", user.getUserId())) {
 							log.debug("用户操作日志记录成功！");
 						}
 						out.print("success");
@@ -82,7 +83,7 @@ public class UserController {
 						String userSessionId = loginUsers.get(user.getLoginName());
 						log.debug("userSessionId:" + userSessionId);
 						log.debug("sessionId:" + sessionId);
-						if(userSessionId == null || !userSessionId.equals(sessionId)) {
+						if (userSessionId == null || !userSessionId.equals(sessionId)) {
 							out.print("登录失败，该用户已登录！");
 						}
 					}
@@ -91,60 +92,63 @@ public class UserController {
 				out.print("登录失败，用户名或者密码错误！");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
 
 	/*
 	 * 用户注销
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/loginOut.do")
-	public String loginOut( HttpServletRequest request, HttpServletResponse response,
+	public String loginOut(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		HttpSession session = request.getSession();
 
 		ServletContext application = request.getSession().getServletContext();
 		@SuppressWarnings("unchecked")
-		Map<String, String> loginUsers = (Map<String, String>) application.getAttribute("loginUsers");
+		Map<String, String> loginUsers = (Map<String, String>) application
+				.getAttribute("loginUsers");
 		User user = (User) session.getAttribute("user");
 		log.debug("loginUsers:" + loginUsers);
-		if(user != null) {
+		if (user != null) {
 			loginUsers.remove(user.getLoginName());
 			application.removeAttribute("loginUsers");
-			application.setAttribute("loginUsers",loginUsers);
+			application.setAttribute("loginUsers", loginUsers);
 		}
 		@SuppressWarnings("unchecked")
 		Enumeration<String> attributeNames = session.getAttributeNames();
-		while(attributeNames.hasMoreElements()) {
+		while (attributeNames.hasMoreElements()) {
 			String attributeName = attributeNames.nextElement();
 			session.removeAttribute(attributeName);
 		}
-		
-		if(saveOprationLog("注销", user.getUserId())) {
+
+		if (saveOprationLog("注销", user.getUserId())) {
 			log.debug("用户操作日志记录成功！");
 		}
 		return "redirect:/admin.do";
 	}
-	
+
 	/*
 	 * 用户修改自己密码
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/updatePwd.do")
-	public String updatePwd( HttpServletRequest request, HttpServletResponse response,
+	public String updatePwd(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		String userPwd = request.getParameter("userPwd");
 		User user = (User) request.getSession().getAttribute("user");
 		user.setUserPwd(userPwd);
-		
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(userService.update(user)) {
-				if(saveOprationLog("修改密码", user.getUserId())) {
+			if (userService.update(user)) {
+				if (saveOprationLog("修改密码", user.getUserId())) {
 					log.debug("用户操作日志记录成功！");
 				}
 				out.print("success");
@@ -152,31 +156,32 @@ public class UserController {
 				out.print("error");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
-	
+
 	/*
 	 * 重置密码
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/resetUserPwd.do")
-	public String resetUserPwd( HttpServletRequest request, HttpServletResponse response,
+	public String resetUserPwd(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		String userPwd = request.getParameter("userPwd");
 		String strId = request.getParameter("userId");
 		long userId = Long.parseLong(strId);
 		User user = (User) userService.getUserById(userId);
 		user.setUserPwd(userPwd);
-		
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(userService.update(user)) {
-				User oprator = (User)request.getSession().getAttribute("user");
-				if(saveOprationLog("重置了登录名为"+ user.getLoginName() + "的用户密码", oprator.getUserId())) {
+			if (userService.update(user)) {
+				User oprator = (User) request.getSession().getAttribute("user");
+				if (saveOprationLog("重置了登录名为" + user.getLoginName() + "的用户密码", oprator.getUserId())) {
 					log.debug("用户操作日志记录成功！");
 				}
 				out.print("success");
@@ -184,17 +189,18 @@ public class UserController {
 				out.print("error");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
-	
+
 	/*
 	 * 修改用户状态
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/changeUserState.do")
-	public String changeUserState( HttpServletRequest request, HttpServletResponse response,
+	public String changeUserState(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		String strUserState = request.getParameter("userState");
 		String strId = request.getParameter("userId");
@@ -202,14 +208,14 @@ public class UserController {
 		int userState = Integer.parseInt(strUserState);
 		User user = (User) userService.getUserById(userId);
 		user.setUserState(userState);
-		
+
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(userService.update(user)) {
-				User oprator = (User)request.getSession().getAttribute("user");
-				if(saveOprationLog("变更了"+ user.getLoginName() + "的状态", oprator.getUserId())) {
+			if (userService.update(user)) {
+				User oprator = (User) request.getSession().getAttribute("user");
+				if (saveOprationLog("变更了" + user.getLoginName() + "的状态", oprator.getUserId())) {
 					log.debug("用户操作日志记录成功！");
 				}
 				out.print("success");
@@ -217,15 +223,14 @@ public class UserController {
 				out.print("error");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
-	
-	
-	
+
 	/*
 	 * 添加用户
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/addUser.do")
@@ -235,9 +240,9 @@ public class UserController {
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(userService.addUser(user)) {
-				User oprator = (User)request.getSession().getAttribute("user");
-				if(saveOprationLog("增加了新用户："+ user.getLoginName() , oprator.getUserId())) {
+			if (userService.addUser(user)) {
+				User oprator = (User) request.getSession().getAttribute("user");
+				if (saveOprationLog("增加了新用户：" + user.getLoginName(), oprator.getUserId())) {
 					log.debug("用户操作日志记录成功！");
 				}
 				out.print("success");
@@ -245,13 +250,14 @@ public class UserController {
 				out.print("error");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
-	
+
 	/*
 	 * 添加用户
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/updateUser.do")
@@ -261,9 +267,9 @@ public class UserController {
 		PrintWriter out;
 		try {
 			out = response.getWriter();
-			if(userService.update(user)) {
-				User oprator = (User)request.getSession().getAttribute("user");
-				if(saveOprationLog("修改了用户："+ user.getLoginName() + "的信息", oprator.getUserId())) {
+			if (userService.update(user)) {
+				User oprator = (User) request.getSession().getAttribute("user");
+				if (saveOprationLog("修改了用户：" + user.getLoginName() + "的信息", oprator.getUserId())) {
 					log.debug("用户操作日志记录成功！");
 				}
 				out.print("success");
@@ -271,46 +277,60 @@ public class UserController {
 				out.print("error");
 			}
 		} catch (IOException e) {
-			log.error("流获取失败！",e);
+			log.error("流获取失败！", e);
 		}
 		return null;
 	}
-	
+
 	/*
 	 * 根据参数列表获取用户信息
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/getUsersByParams.do")
-	public String getUsersByParams( HttpServletRequest request, HttpServletResponse response,
+	public String getUsersByParams(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		String keyWord = request.getParameter("keyWord");
 		String strCompanyId = request.getParameter("companyId");
 		String strUserType = request.getParameter("userType");
 		String strUserState = request.getParameter("userState");
-		
+		System.out.println("userType====" + strUserType);
+		System.out.println("userState====" + strUserState);
+
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("keyWord", keyWord);
-		if(strCompanyId != null && strCompanyId.length() > 0 && strCompanyId.matches("^[0-9]*$")) {
+		if (strCompanyId != null && strCompanyId.length() > 0 && strCompanyId.matches("^[0-9]*$")) {
 			params.put("companyId", new Long(strCompanyId));
+		} else {
+			params.put("companyId", null);
 		}
-		if(strUserType != null && strUserType.length() > 0 && strUserType.matches("^[0-9]*$")) {
+		if (strUserType != null && strUserType.length() > 0 && strUserType.matches("^[0-9]*$")) {
 			params.put("userType", new Integer(strUserType));
+		} else {
+			params.put("userType", null);
 		}
-		if(strUserState != null && strUserState.length() > 0 && strUserState.matches("^[0-9]*$")) {
+		if (strUserState != null && !strUserState.equals("") && strUserState.length() > 0
+				&& strUserState.matches("^[0-9]*$")) {
 			params.put("userState", new Integer(strUserState));
+		} else {
+			params.put("userState", null);
 		}
+
+		System.out.println("userType====" + params.get("userType"));
+		System.out.println("userState====" + params.get("userState"));
 		List<User> userList = userService.getUsersByParams(params);
 		modelMap.put("userList", userList);
-		
+
 		return "/jsp/admin/pages/usersAdmin";
 	}
-	
+
 	/*
 	 * 根据参数列表获取日志信息
+	 * 
 	 * @author cj
 	 */
 	@RequestMapping("/getLogByParams.do")
-	public String getLogByParams( HttpServletRequest request, HttpServletResponse response,
+	public String getLogByParams(HttpServletRequest request, HttpServletResponse response,
 			ModelMap modelMap) {
 		String logDate = request.getParameter("logDate");
 		User user = (User) request.getSession().getAttribute("user");
@@ -319,17 +339,17 @@ public class UserController {
 		params.put("oprator", user.getUserId());
 		List<OprationLog> logList = oprationLogService.getLogByParams(params);
 		modelMap.put("logList", logList);
-		
+
 		return "/jsp/admin/pages/showLog";
 	}
-	
+
 	private boolean saveOprationLog(String msg, long opratorId) {
 		OprationLog log = new OprationLog();
 		log.setLogDate(new Date());
 		log.setOpration(msg);
 		log.setOprator(opratorId);
-		
+
 		return oprationLogService.addLog(log);
 	}
-	
+
 }
