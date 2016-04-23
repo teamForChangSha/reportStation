79fd52882b5adfbea7d246b6307442fbc9feb3ad
git123xia <820786562@qq.com>
2016-04-23 11:56:19 +0800

    测试类更新

--- src/java/test/com/jxxp/test/mybatis/CaseTest.java
+++ src/java/test/com/jxxp/test/mybatis/CaseTest.java
@@ -123,7 +123,8 @@ public class CaseTest {
 		Date createTime = caseInfo.getCreateTime();
 		List<ReportCase> caseList = reportCaseMapper.searchByKeys(caseInfo.getCompany()
 				.getCompanyId(), format.format(createTime), fullFormat.format(createTime), answer
-				.getQuestValue(), caseInfo.getRtList(), caseInfo.getCaseState());
+				.getQuestValue(), caseInfo.getRtList(), caseInfo.getCaseState(), caseInfo
+				.getTrackingNo());
 		System.out.println(caseList.size());
 		assertTrue(caseList.size() == 1);
 	}
@@ -144,7 +145,7 @@ public class CaseTest {
 		List<ReportCase> caseList1 = reportCaseMapper.getAllByCompanyId(caseInfo.getCompany()
 				.getCompanyId());
 		List<ReportCase> caseList2 = reportCaseMapper.searchByKeys(caseInfo.getCompany()
-				.getCompanyId(), "", "2016-02-28", null, null, null);
+				.getCompanyId(), "", "2016-02-28", null, null, null, null);
 		assertTrue(caseList2.size() >= 1);
 		assertTrue(caseList1.size() == caseList2.size());
 
@@ -164,7 +165,7 @@ public class CaseTest {
 		List<ReportCase> caseList1 = reportCaseMapper.getAllByCompanyId(caseInfo.getCompany()
 				.getCompanyId());
 		List<ReportCase> caseList2 = reportCaseMapper.searchByKeys(caseInfo.getCompany()
-				.getCompanyId(), null, null, null, null, null);
+				.getCompanyId(), null, null, null, null, null, null);
 		assertTrue(caseList2.size() >= 1);
 		assertTrue(caseList1.size() == caseList2.size());
 
